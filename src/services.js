4e8fc12f898aa7e80d0b8e8d98e94ced0b09b0d5
jessito <jessica.torres.c@hotmail.com>
2025-07-10 12:10:15 +0200

    create function method post(#4)

--- src/services.js
+++ src/services.js
@@ -40,10 +40,43 @@ const printFilms = async () => {
         <p><b>Descripción de la pelicla</b>: ${film.description}</p>
         <p><b>numero de la pelicula</b>: ${film.id}</p>
         </div>`
-        
+
     });
 }
 printFilms();
 // ========================================
 //  CREATE
 // ========================================
+//creamos una funcion para añadir peliculas nuevas: const createFilm
+//newFilm: contiene la información de la pelicula nueva
+const createFilm = async (newFilm) => {
+    //hace una petición a la misma URL, pero con configuracion especial para enviar datos
+    //¿Por qué es diferente a la función READ? 
+    //En READ solo pedíamos datos (GET)
+    //En CREATE vamos a enviar datos nuevos (POST)
+    const response = await fetch(URL_API_FILMS, {
+        //le dice al servidor que queremos ENVIAR datos nuevos
+        //POST: es el tipo de petición HTTP para crear/añadir cosas nuevas
+        //POST(toma estos datos nuevos)
+        method: "POST",
+        //abre una sección para configurar como enviar los datos
+        //headers: son como etiquetas que le dicen al servidor que tipo de datos le enviamos
+        headers: {
+         //Content-Type le dice al servidor que formto tienen los datos
+         //application/json" = Especifica que los datos están en formato JSON
+            "Content-Type": "application/json"
+        },
+        //envia los datos de la pelicula nueva convertidos a formato JSON
+        //body: = Aquí van los datos que queremos enviar al servidor
+        //JSON.stringify(newFilm) = Convierte el objeto newFilm a formato JSON (texto)
+        //newFilm = Los datos de la película que recibimos como parámetro
+        body: JSON.stringify(newFilm)
+
+    });
+    //response.json() = Convierte la respuesta del servidor a formato JavaScript
+    //await = Espera a que termine la conversión
+    //const createdFilm = Guarda la película creada (con el ID que le asignó el servidor)
+    const createdFilm = await response.json();
+    //"Devuelve la película recién creada con toda su información
+    return createdFilm;
+}
